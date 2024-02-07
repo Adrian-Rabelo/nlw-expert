6f916fd504df91558ea8aa22352ca7d76349c35f
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2024-02-07 17:36:49 -0400

    aula 02 - banco de dados

--- src/main/java/com/rabelinho/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rabelinho/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,21 @@
 package com.rabelinho.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rabelinho.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.rabelinho.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
-  public boolean execute( VerifyIfHasCertificationDTO dto ) {
-    if (dto.getEmail().equals("adrianrabeloe10@gmail.com") && dto.getTechnology().equals("Java")) {
+  @Autowired
+  private CertificationStudentRepository certificationStudentRepository;
+
+  public boolean execute(VerifyIfHasCertificationDTO dto) {
+    var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+        dto.getTechnology());
+    if (!result.isEmpty()) {
       return true;
     }
     return false;
