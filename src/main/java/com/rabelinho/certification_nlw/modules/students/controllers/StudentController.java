3c3acaa3c06b70618a2502489bec63ef083fe749
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2024-02-09 11:08:25 -0400

    aula 3

--- src/main/java/com/rabelinho/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/rabelinho/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,12 +1,16 @@
 package com.rabelinho.certification_nlw.modules.students.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rabelinho.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
+import com.rabelinho.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
 import com.rabelinho.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.rabelinho.certification_nlw.modules.students.entities.CertificationStudentEntity;
 import com.rabelinho.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 
 @RestController
@@ -15,6 +19,9 @@ public class StudentController {
 
   @Autowired
   private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+  @Autowired
+  private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
   
   @PostMapping("/verifyIfHasCertification")
   public String verifyIfHasCertification(@RequestBody VerifyIfHasCertificationDTO verifyHasCertificationDTO) {
@@ -26,5 +33,17 @@ public class StudentController {
     }
 
     return "Usuário pode fazer a prova";
+  }
+
+  @PostMapping("/certification/answer")
+  public ResponseEntity<Object> certificationAnswer(@RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
+
+    try {
+        var result = studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+        return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
   }
 }
--- src/main/java/com/rabelinho/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/rabelinho/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -6,6 +6,10 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,10 +20,12 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "certifications")
@@ -42,8 +48,9 @@ public class CertificationStudentEntity {
   @JoinColumn(name = "student_id", insertable = false, updatable = false)
   private StudentEntity studentEntity;
 
-  @OneToMany
+  @OneToMany(cascade = CascadeType.ALL)
   @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+  @JsonManagedReference
   List<AnswersCertificationsEntity> answersCertificationsEntity;
 
   @CreationTimestamp
